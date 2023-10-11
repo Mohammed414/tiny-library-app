992b35eb06631cd401712fb2e60b146014de051d
Mohammed M. Hilal <mhilaldev@gmail.com>
2023-10-12 01:43:36 +0300

    finish building the app

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const myLibrary = [
+// get table body element
+const tableBody = document.querySelector("tbody");
+
+let myLibrary = [
   {
     title: "The Hobbit",
     author: "J.R.R. Tolkien",
@@ -12,6 +15,15 @@ const myLibrary = [
     isRead: true,
   },
 ];
+
+// check if myLibrary is in local storage
+if (localStorage.getItem("myLibrary") === null) {
+  // save myLibrary to local storage
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+} else {
+  // sync myLibrary with local storage
+  myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+}
 
 function Book(title, author, numberOfPages, isRead) {
   this.title = title;
@@ -28,11 +40,14 @@ function Book(title, author, numberOfPages, isRead) {
 function addBookToLibrary(book) {
   // store book in myLibrary
   myLibrary.push(book);
+  // update local storage
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  updateLibrary();
 }
 
-function getTableRow(no, title, author, numberOfPages, isRead) {
-  return `<tr>
-  <td class="book-no">${no}</td>
+function getTableRow(index, title, author, numberOfPages, isRead) {
+  return `<tr data-index="${index}" >
+  <td class="book-no">${index + 1}</td>
   <td class="book-title">${title}</td>
   <td class="book-author">${author}</td>
   <td class="book-nop">${numberOfPages}</td>
@@ -40,21 +55,101 @@ function getTableRow(no, title, author, numberOfPages, isRead) {
     isRead ? "Read" : "Not Read"
   }</td>
   <td class="delete-cell">
-    <button class="delete">Delete</button>
+    <button class="btn btn-danger delete-cell-button">Delete</button>
   </td>
   </tr>`;
 }
+function updateLibrary() {
+  // clear table
+  tableBody.innerHTML = "";
+  // add books to table
+  JSON.parse(localStorage.getItem("myLibrary")).forEach((book, index) => {
+    tableBody.innerHTML += getTableRow(
+      index,
+      book.title,
+      book.author,
+      book.numberOfPages,
+      book.isRead
+    );
+  });
+  // add event listener to delete buttons
+  let deleteCellButtons = document.querySelectorAll(".delete-cell-button");
+  deleteCellButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let index = event.target.parentElement.parentElement.dataset.index;
+      deleteBook(index);
+    });
+  });
 
-// get table body element
-const tableBody = document.querySelector("tbody");
+  // add event listener to status cells
+  let statusCells = document.querySelectorAll(".book-status");
+  statusCells.forEach((cell) => {
+    cell.addEventListener("click", (event) => {
+      cell.classList.toggle("read");
+      cell.classList.toggle("not-read");
+      cell.textContent = cell.textContent === "Read" ? "Not Read" : "Read";
+      let index = event.target.parentElement.dataset.index;
+      myLibrary[index].isRead = !myLibrary[index].isRead;
+      // update local storage
+      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    });
+  });
+}
 
-// add books to table
-myLibrary.forEach((book, index) => {
-  tableBody.innerHTML += getTableRow(
-    index + 1,
-    book.title,
-    book.author,
-    book.numberOfPages,
-    book.isRead
-  );
+function deleteBook(index) {
+  // delete book from myLibrary
+  myLibrary.splice(index, 1);
+  // update local storage
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  updateLibrary();
+}
+// start
+updateLibrary();
+
+// Dialog
+let openDialogButton = document.querySelector("#open-dialog");
+let dialog = document.querySelector(".dialog");
+let confirmButton = document.querySelector("#confirm-button");
+let closeDialogButton = document.querySelector("#close-dialog");
+const authorInput = document.querySelector("#author");
+const titleInput = document.querySelector("#title");
+const numberOfPagesInput = document.querySelector("#pages");
+
+openDialogButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialog.addEventListener("close", () => {
+  console.log(dialog.returnValue === "");
+  if (dialog.returnValue === "") {
+    return;
+  }
+
+  authorInput.value = "";
+  titleInput.value = "";
+  numberOfPagesInput.value = "";
+  let newBook = JSON.parse(dialog.returnValue);
+  addBookToLibrary(newBook);
+});
+
+closeDialogButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+confirmButton.addEventListener("click", (even) => {
+  even.preventDefault();
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let numberOfPages = numberOfPagesInput.value;
+  if (title === "" || author === "" || numberOfPages === "") {
+    alert("Please fill all the fields");
+    return;
+  }
+  let isRead =
+    document.querySelector('input[name="status"]:checked').value === "read"
+      ? true
+      : false;
+
+  // stringfy the book object and pass it to the close method
+  dialog.close(JSON.stringify(new Book(title, author, numberOfPages, isRead)));
 });
